3e4f69cd15fe6d3a74d77b24b6de98896b6be4fa
Kaneis Zontanos <ramim@bc.edu>
2024-12-01 17:56:35 -0500

    adding binaryView

---
+++ BinaryPuzzle/BinaryView.swift
@@ -0,0 +1,20 @@
+//
+//  BinaryView.swift
+//  BinaryPuzzle
+//
+//  Created by Kaneis Zontanos on 12/1/24.
+//
+
+import SwiftUI
+
+struct BinaryView: View {
+    var body: some View {
+        Text("Aim")
+        
+        Text("BUttons")
+    }
+}
+
+#Preview {
+    BinaryView()
+}
