1e6116488a95e5d8ae3e710ec8e2e04774ba1d46
Kaneis Zontanos <ramim@bc.edu>
2024-12-01 17:32:52 -0500

    Initial Commit

---
+++ BinaryPuzzle/BinaryPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinaryPuzzleApp.swift
+//  BinaryPuzzle
+//
+//  Created by Kaneis Zontanos on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct BinaryPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BinaryPuzzle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BinaryPuzzle
+//
+//  Created by Kaneis Zontanos on 12/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
